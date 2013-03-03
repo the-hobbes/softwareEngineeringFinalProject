810484fa9a8f8eeb0039a5fe2123d6b92e1814da
the-hobbes <phelan.vendeville@gmail.com>
2013-03-03 15:23:10 -0500

    added scripts directory to the yaml config file. worked on landing, character choice, and user input pages. also modified their corresponding handlers. added lightbox functionality and styling for all pages.

--- rat-cat/main.py
+++ rat-cat/main.py
@@ -19,49 +19,17 @@
 # 	Ethan, Phelan
 # 
 # This is the main app driver file. Requests will hit here when users first enter the page
-
-import webapp2
+from python.handler import *
 from python.game import *
 from python.scores import *
 from python.playerinfo import *
 from python.difficulty import *
 from python.characterchoice import *
-import os
-import jinja2
-import re
-from google.appengine.ext import db
-
-#set templating directory with jinja. NOTE that jinja escapes html because autoescape = True
-template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
-
-class Handler(webapp2.RequestHandler):
-	'''
-		Class Handler 
-		This class inherits from RequestHandler. It is used to simplify writing and rendering to templates by providing
-		simpler methods to use in order to do such operations. 
-		For example, in other classes which implement this Handler, rendering data to a template can be accomplished by 
-		the following statement:
-			self.render(template, keyvalue pairs)
-		as opposed to the following: 
-			self.write(self.render_str(template, keyvalue pairs))
-		params: inherits from RequestHandler
-	'''
-	def write(self, *a, **kw):
-		self.response.out.write(*a, **kw)
-
-	def render_str(self, template, **params):
-		t = jinja_env.get_template(template)
-		return t.render(params)
-
-	def render(self, template, **kw):
-		#called by render_front in MainPage class
-		self.write(self.render_str(template, **kw))
 
 class MasterControlProgram(Handler):
 	'''
 		Class MasterControlProgram
-		Inherits from the Handler class to facilitate writing to templates. 
+		Inherits from the Handler class (in python directory) to facilitate writing to templates. 
 		Used to handle all http requests for the root of the site. 
 	'''
 	def get(self):
--- rat-cat/python/characterchoice.py
+++ rat-cat/python/characterchoice.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python
+# Created 3MAR2013 
+# Authors:
+# 	Phelan
+# 
+# This handler facilitates choosing the player character. It renders a template that allows the user to choose one of 6 
+# characters, and then forwards the user to the next screen with that information. 
 
-import webapp2
+from handler import *
 
-class CharacterHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello character choice!')
+class CharacterHandler(Handler):
+	def get(self):
+		'''
+			get
+			Standard get method. Renders template.
+		'''
+		self.render("choosePlayer.html")
+	
+	def post(self):
+		'''
+			post
+			Standard post method. Collects posted form information.
+		'''
+		radioAnswer = self.request.get("characterGroup")
+		self.write(radioAnswer)
