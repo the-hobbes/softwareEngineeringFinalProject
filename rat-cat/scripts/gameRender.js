b31f99de08bc540913f201cf6592291c2309c658
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-03-14 20:08:07 -0700

    Created render state for initial, still need to do the one where we update using both old and new states. Still need to add in some glows and such

--- rat-cat/scripts/gameRender.js
+++ rat-cat/scripts/gameRender.js
@@ -5,6 +5,7 @@
  *
  */
 
+
 /*
  *renderState: This function takes the state of the game and it's transitioning state and 
  *			merges the two into the new state of the game. This functions depends on the 
@@ -12,19 +13,53 @@
  *				opCard1,opCard2,opCard3,opCard4,discardPile,deck,currentCard,knockButton,playerCard1,playerCard2,playerCard3,playerCard4
  *	state: The JSON representative of the game board, this is a JSON
  *		   object which has the following high level pairs:
- *		   { "Deck" : {}, "Discard" : {}, "CompCard" : {}, "PlayCard" : {},
- * 			 "DisplayCard" : {}, "KnockState" : 0 | 1, "State" : "state specified by specs" }			 
+ *		   { "deck" : {}, "discard" : {}, "compCard" : {}, "playCard" : {},
+ * 			 "displayCard" : {}, "knockState" : 0 | 1, "state" : "state specified by specs" }			 
  *	returns: the new state of the game
  */
  function renderState(oldState,newState){
  	if(oldState == null){
  		//Initializing the game so the newState is correct
- 		return newState;	
+ 		//TODO: We need to decide the first state of the game and glow appropriate things
+ 		
+ 		//Read each state and put the proper image out for the opponent cards:
+ 		for(var i=0; i < 4; i++){
+ 			var imgId = newState.compCard[i].image;
+ 			$('#opCard' + (i + 1)).css("background-image", 'url(' + "images/cards/smallCards/"+ imgId + ".png" + ')' );
+ 		}
+
+ 		//Update the images for the player Cards
+ 		for(var i=0; i < 4; i++){
+ 			var imgId = newState.playCard[i].image;
+ 			$('#opCard' + (i + 1)).css("background-image", 'url(' + "images/cards/smallCards/"+ imgId + ".png" + ')' );
+ 		}
+
+ 		//Show the discard pile card:
+ 		if(newState.discard[0] == null){
+ 			$('#discardPile').css("background-image", 'url(images/placeholderCard.png)');
+ 		}else{
+ 			$('#discardPile').css("background-image", 'url(images/cards/smallCards/' + newState.discard[0] + '.png)');
+ 		}
+
+ 		//Display either the bottom of a card or a placeholder if we managed to run out of deck cards
+ 		if(newState.deck[0] == null){
+ 			$('#deck').css("background-image", 'url(images/placeholderCard.png)');
+ 		}else{
+ 			$('#deck').css("background-image", 'url(images/cards/smallCards/13.png)');
+ 		}
+
+ 		//Display whatever the currently displayed card is
+ 		$('#currentCard').css("background-image", "url(images/cards/" + newState.displayCard.image + ".png)");
+
+ 		//The knock state should be 0 because we're not ending the game yet
+ 		newState.knockState = 0;
+
+ 		//Set the initial state
+ 		newState.state = "waitingForDraw";
+
+ 		return newState;
  	}
- 	alert(oldState["KnockState"]);
 
-
-
-
+ 	
 
  }
