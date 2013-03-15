5247de5f26e44c8332cc4fb8f9769ffe8f24a9c8
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-03-14 22:46:21 -0700

    added switch handler for state

--- rat-cat/scripts/stateChanges.js
+++ rat-cat/scripts/stateChanges.js
@@ -13,7 +13,8 @@
  *	returns: The updated state
  */
 function waitingForDraw(state){
-
+	alert('waitingForDraw state');
+	return state;
 }
 
 
@@ -25,7 +26,8 @@ function waitingForDraw(state){
  *	returns: The updated state
  */
 function waitingForPCard(state){
-
+	alert('waitingForPCard state');
+	return state;
 }
 
 
@@ -37,7 +39,8 @@ function waitingForPCard(state){
  *	returns: The updated state
  */
 function HAL(state){
-
+	alert('HAL state');
+	return state;
 }
 
 /*playerChoice: This function causes the board to update to the right glowing state
@@ -48,7 +51,8 @@ function HAL(state){
  *	returns: The updated state
  */
 function playerChoice(state){
-
+	alert('playerChoice state');
+	return state;
 }
 
 /*draw2PlayerChoice: This function causes the board to update to the right glowing state
@@ -59,5 +63,30 @@ function playerChoice(state){
  *	returns: The updated state
  */
 function draw2PlayerChoice(state){
+	alert('draw2PlayerChoice state');
+	return state;
+}
 
+/*handleState: This function passes the state through a switch statement and calls the proper rendering function
+ *	state: The JSON representative of the game board, this is a JSON
+ *		   object which has the following high level pairs:
+ *		   { "deck" : {}, "discard" : {}, "compCard" : {}, "playCard" : {},
+ * 			 "displayCard" : {}, "knockState" : 0 | 1, "state" : "state specified by specs" }
+ *	returns: The updated state
+ */
+function handleState(state){
+	switch( state.state){
+		case 'waitingForDraw':
+			return waitingForDraw(state);
+		case 'waitingForPCard':
+			return waitingForPCard(state);
+		case 'HAL':
+			return HAL(state);
+		case 'playerChoice':
+			return playerChoice(state);
+		case 'draw2PlayerChoice':
+			return draw2PlayerChoice(state);
+		default:
+			return state;
+	}
 }
