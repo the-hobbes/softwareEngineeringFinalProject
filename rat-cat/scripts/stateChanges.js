21c185fbccf91335e08ab2d62848f019b6f65e5c
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-03-15 00:01:09 -0700

    added animation to the player's cards on initial state to show the player their cards in a nice fancy way. Need to work on delaying transition between states. Need to work on renderState as well for the case where old and new state are defined

--- rat-cat/scripts/stateChanges.js
+++ rat-cat/scripts/stateChanges.js
@@ -5,6 +5,42 @@
  *
  */
 
+
+/*initialState: This function shows the player their cards for a brief time and then transitions 
+ *			to the next state
+ *	state: The JSON representative of the game board, this is a JSON
+ *		   object which has the following high level pairs:
+ *		   { "deck" : {}, "discard" : {}, "compCard" : {}, "playCard" : {},
+ * 			 "displayCard" : {}, "knockState" : 0 | 1, "state" : "state specified by specs" }
+ *	returns: The updated state
+ */
+function initialState(state){
+	//alert('initialState');
+
+	//Any fancy animations for shuffling and dealing go here, such as tweening cards from the deck to the players hands
+
+	//Show the player their two outermost cards for a short time:
+	$('#playerCard1').css("background-image", 'url(images/cards/smallCards/'+ state.playCard[0].image +'.png)');
+	$('#playerCard4').css("background-image", 'url(images/cards/smallCards/'+ state.playCard[3].image +'.png)');
+
+	//The function below fades out the card and then fades it back in
+	$('#playerCard1').animate({opacity : 0},4000,function(){ $('#playerCard1').animate({opacity : 1},1000)});
+	$('#playerCard4').animate({opacity : 0},4000,function(){ $('#playerCard4').animate({opacity : 1},1000)});
+
+	//This function 'hides' the cards again
+	setTimeout(function(){
+		$('#playerCard1').css("background-image", 'url(images/cards/smallCards/13.png)');
+		$('#playerCard4').css("background-image", 'url(images/cards/smallCards/13.png)');
+	},5000);
+
+	//No real need for an ajax call we'll just update things here
+	state.state = 'waitingForDraw';
+
+	return handleState(state);
+
+}
+
+
 /*waitingForDraw: This function causes the board to update to the right glowing state
  *			Which means glowing the discard and deck
  *	state: The JSON representative of the game board, this is a JSON
@@ -14,16 +50,33 @@
  *	returns: The updated state
  */
 function waitingForDraw(state){
-	alert('stateChange.js 17: waitingForDraw state');
+	//alert('stateChange.js : waitingForDraw state');
 
+	//Add glow to whatever the user will interact with
 	$('#deck').addClass('glowing');
 	$('#discardPile').addClass('glowing');
 
+	//This actually causes the glow
 	var glow = $('.glowing');
-	
 	setInterval(function(){
 	    glow.hasClass('glow') ? glow.removeClass('glow') : glow.addClass('glow');
 	}, 2000);
+
+	//Add listeners for clicks that will fire off whatever interaction we need
+	//and will also remove the glow
+	$('#deck').click(function(){
+		//Use ajax to yell over to the server that something has happened
+
+		//Remove the glow
+		$(this).removeClass('glowing');
+	});
+
+	$('#discardPile').click(function(){
+		//Use ajax to yell over to the server that something has happened
+
+		//Remove the glow
+		$(this).removeClass('glowing');
+	});
 
 	return state;
 }
@@ -37,7 +90,7 @@ function waitingForDraw(state){
  *	returns: The updated state
  */
 function waitingForPCard(state){
-	alert(' stateChange.js 40: waitingForPCard state');
+	alert(' stateChange.js : waitingForPCard state');
 	return state;
 }
 
@@ -97,6 +150,8 @@ function handleState(state){
 			return playerChoice(state);
 		case 'draw2PlayerChoice':
 			return draw2PlayerChoice(state);
+		case 'initial':
+			return initialState(state);
 		default:
 			return state;
 	}
