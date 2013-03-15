19783e6a87258b8d99b750e5034814921c393a80
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-03-14 21:56:27 -0700

    Added javascript shell file for functions to change the view of the game board per state

---
+++ rat-cat/scripts/stateChanges.js
@@ -0,0 +1,63 @@
+/*
+ * Created 14MAR2013 
+ * Authors:
+ * 	Ethan
+ *
+ */
+
+/*waitingForDraw: This function causes the board to update to the right glowing state
+ *	state: The JSON representative of the game board, this is a JSON
+ *		   object which has the following high level pairs:
+ *		   { "deck" : {}, "discard" : {}, "compCard" : {}, "playCard" : {},
+ * 			 "displayCard" : {}, "knockState" : 0 | 1, "state" : "state specified by specs" }
+ *	returns: The updated state
+ */
+function waitingForDraw(state){
+
+}
+
+
+/*waitingForPCard: This function causes the board to update to the right glowing state
+ *	state: The JSON representative of the game board, this is a JSON
+ *		   object which has the following high level pairs:
+ *		   { "deck" : {}, "discard" : {}, "compCard" : {}, "playCard" : {},
+ * 			 "displayCard" : {}, "knockState" : 0 | 1, "state" : "state specified by specs" }
+ *	returns: The updated state
+ */
+function waitingForPCard(state){
+
+}
+
+
+/*HAL: This function causes the board to update to the right glowing state
+ *	state: The JSON representative of the game board, this is a JSON
+ *		   object which has the following high level pairs:
+ *		   { "deck" : {}, "discard" : {}, "compCard" : {}, "playCard" : {},
+ * 			 "displayCard" : {}, "knockState" : 0 | 1, "state" : "state specified by specs" }
+ *	returns: The updated state
+ */
+function HAL(state){
+
+}
+
+/*playerChoice: This function causes the board to update to the right glowing state
+ *	state: The JSON representative of the game board, this is a JSON
+ *		   object which has the following high level pairs:
+ *		   { "deck" : {}, "discard" : {}, "compCard" : {}, "playCard" : {},
+ * 			 "displayCard" : {}, "knockState" : 0 | 1, "state" : "state specified by specs" }
+ *	returns: The updated state
+ */
+function playerChoice(state){
+
+}
+
+/*draw2PlayerChoice: This function causes the board to update to the right glowing state
+ *	state: The JSON representative of the game board, this is a JSON
+ *		   object which has the following high level pairs:
+ *		   { "deck" : {}, "discard" : {}, "compCard" : {}, "playCard" : {},
+ * 			 "displayCard" : {}, "knockState" : 0 | 1, "state" : "state specified by specs" }
+ *	returns: The updated state
+ */
+function draw2PlayerChoice(state){
+
+}
