6de3d91d4dcf1715f31ea516e33575fbcb4989bd
the-hobbes <phelan.vendeville@gmail.com>
2013-03-14 22:54:10 -0400

    added initEncode method to game.py handler so that an initial state is encoded and passed to the javascript model. TODO: the json object is htmlescaped twice, so i must find a way around that

--- rat-cat/python/game.py
+++ rat-cat/python/game.py
@@ -5,6 +5,8 @@
 # 
 # This handler executes the actual game environment. 
 from handler import *
+from random import shuffle
+import json
 
 class GameHandler(Handler):
 	'''
@@ -14,15 +16,54 @@ class GameHandler(Handler):
 	'''
 	def get(self):
 		'''
-		get
-		You got your basic get method right here.
+			get
+			You got your basic get method right here, but with a twist. 
+			This method perfoms the intialization of the game state, creating the json objects that represent the game and
+			json encoding them. It then renders them on the game template with jinja. 
 		'''
-		self.render("game.html")
+		#perform initial creation and encoding of JSON object
+		newState = self.initEncode()
+		self.render("game.html", oldState='null', newState=newState)
+
+		# TODO
+		# the json object is showing up weirdly because it is being escaped twice (once by json.dumps and once by autoescape =true)
+		# got to find a way around that
+		# also, need to massage ethan's script so it can deal with the 'visible' property i've added
 
 	def post(self):
 		'''
-		post
-		Responds to post requests for the resource.
+			post
+			Responds to post requests for the resource.
 		'''
 		self.write("You posted. Good for you.")
 
+	def initEncode(self):
+		'''
+			initEncode
+			Creates the initial JSON key:value pairs and encodes them to be sent to the game template.
+			Returns:
+				initialState, the initial state of the gameboard
+		'''
+		# make a list of lists of cards, flatten it, then shuffle it
+		listOfLists = [ [0]* 4, [1]*4, [2]*4, [3]*4, [4]*4, [5]*4, [6]*4, [7]*4, [8]*4, [9]*9, [10]*3, [11]*3, [12]*3 ]
+		deck = sum(listOfLists, [])
+		shuffle(deck)
+
+		newState = {"compCard" : [
+						{"image" : str(deck.pop()), 'active' : 0, 'visible' : 0}, 
+						{'image' : str(deck.pop()), 'active' : 0, 'visible' : 0}, 
+						{'image' : str(deck.pop()), 'active' : 0, 'visible' : 0},
+						{'image' : str(deck.pop()), 'active' : 0, 'visible' : 0}],  
+					"playCard" : [
+						{'image' : str(deck.pop()), 'active' : 0, 'visible' : 1}, 
+						{'image' : str(deck.pop()), 'active' : 0, 'visible' : 0}, 
+						{'image' : str(deck.pop()), 'active' : 0, 'visible' : 0},
+						{'image' : str(deck.pop()), 'active' : 0, 'visible' : 1}], 
+					"discard" : [], 
+					"deck" : deck,
+					"displayCard" : {'image' : "13", 'active' : 0}, 
+					"knockState" : 0, 
+					"state" : "waitingForDraw" 
+				}
+		# encode it
+		return json.dumps(newState)
