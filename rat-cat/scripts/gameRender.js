9b28ab008bd5f15891fec61e2f208a328afff467
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-03-14 19:02:07 -0700

    Updated comment so I can know which id's there are

--- rat-cat/scripts/gameRender.js
+++ rat-cat/scripts/gameRender.js
@@ -6,15 +6,25 @@
  */
 
 /*
- *renderState This will 
+ *renderState: This function takes the state of the game and it's transitioning state and 
+ *			merges the two into the new state of the game. This functions depends on the 
+ *			following ID's being present on the game.html page: 
+ *				opCard1,opCard2,opCard3,opCard4,discardPile,deck,currentCard,knockButton,playerCard1,playerCard2,playerCard3,playerCard4
  *	state: The JSON representative of the game board, this is a JSON
  *		   object which has the following high level pairs:
  *		   { "Deck" : {}, "Discard" : {}, "CompCard" : {}, "PlayCard" : {},
- 			 "DisplayCard" : {}, "KnockState" : 0 | 1, "State" : "state specified by specs" }
+ * 			 "DisplayCard" : {}, "KnockState" : 0 | 1, "State" : "state specified by specs" }			 
  *	returns: the new state of the game
  */
  function renderState(oldState,newState){
- 	//null of oldstate means initializeing bitch
+ 	if(oldState == null){
+ 		//Initializing the game so the newState is correct
+ 		return newState;	
+ 	}
+ 	alert(oldState["KnockState"]);
+
+
+
 
 
  }
