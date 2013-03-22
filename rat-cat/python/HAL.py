8f4c7a2c0bd6719fe48d1d79f02c74468392cabb
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-03-22 00:25:45 -0700

    Updated property types and began working on constructors

--- rat-cat/python/HAL.py
+++ rat-cat/python/HAL.py
@@ -23,22 +23,24 @@ class HAL(db.Model):
 		Difficulty of 3 means no recay rate and perfect recall. Good luck.
 	'''
 	#Estimates used in decision calculations
+	pkSessionID = db.StringProperty(required=True)
 	estAIScore = db.IntegerProperty(required=True)
 	estOppScore = db.IntegerProperty(required=True)
-	#CSV for memory values 
-	opCardsMem = db.StringProperty(required=True)
-	aiCardsMem = db.StringProperty(required=True)
-	#CSV for the actual card values we know
-	opCards = db.StringProperty(required=True)
-	aiCards = db.StringProperty(required=True)
+	#list for memory values 
+	opCardsMem = db.ListProperty(float,required=True)
+	aiCardsMem = db.ListProperty(float,required=True)
+	#list for the actual card values we know
+	opCards = db.ListProperty(int,required=True)
+	aiCards = db.ListProperty(int,required=True)
 	discardTopValue = db.IntegerProperty(required=True)
 	#Memory decay rate, abs and rounded between 0.01-.99
 	decayRate = db.FloatProperty(required=True)
-	#decayMemory CSV, represents chance of remembering correctly
-	decayMemory = StringProperty(required=True)
-	#Statics to log?
+	#decayMemory list, represents chance of remembering correctly
+	decayMemory = db.ListProperty(float,required=True)
+	#This will be a string that builds up to tell the view what to do
+	actionsToTake = ""
 
-	def __init__(self,sessionID="Debug",diff=0,aiCardArray=[]):
+	def __init__(self,sessionID="Debug",diff=0,aiCardArray=[9,9,9,9],topValue=9):
 		'''
 			Constructor
 				Creates the HAL instance in the database to keep persistent state. 
@@ -51,6 +53,17 @@ class HAL(db.Model):
 					Difficulty of 3 corresponds to perfect memory recall (Not selectable by user but for funsies is here)
 				aiCardArray, An array of the AI's initial cards
 		'''
+		self.key_name=sessionID
+		self.pkSessionID=sessionID
+		self.estAIScore=36
+		self.estOppScore=18
+		opCardsMem=[1.0,1.0,1.0,1.0]
+		aiCardsMem=[1.0,1.0,1.0,1.0]
+		opCards=[0,9,9,0]
+		aiCards=aiCardArray
+		discardTopValue=topValue
+		decayRate = 0 #use some equation of difficult (probs 1/e^diff and some stuff)
+		decayMemory = [1.0,1.0,1.0,1.0]
 
 
 
