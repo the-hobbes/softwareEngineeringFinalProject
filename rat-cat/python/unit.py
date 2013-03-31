626b91d09801b1dab1d4eecf0bedeb1a71c10935
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-03-30 20:40:51 -0700

    Began working in Unit Harness handler that will perform testing upon each python file and output the results

---
+++ rat-cat/python/unit.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# Created 3MAR2013 
+# Authors:
+# 	Ethan
+# 
+# This handler facilitates choosing the player character. It renders a template that allows the user to choose one of 6 
+# characters, and then forwards the user to the next screen with that information. 
+
+from handler import *
+
+class UnitHarness(Handler):
+	'''
+		UnitHarness
+		Inherits from handler. Used to respond to http requests for the /unit url
+		Performs basic Unit tests on each python file. Generates statistics and information for each
+	'''
+	def get(self):
+		'''
+			get
+			Standard get method. Renders template.
+		'''
+		self.render("unit.html")
+	
