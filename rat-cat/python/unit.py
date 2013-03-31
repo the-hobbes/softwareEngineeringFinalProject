9c9df64a17dc0886fcecd178ec71af0436c053dc
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-03-30 21:57:35 -0700

    Added unit test for timing on scores.py

--- rat-cat/python/unit.py
+++ rat-cat/python/unit.py
@@ -7,6 +7,8 @@
 # characters, and then forwards the user to the next screen with that information. 
 
 from handler import *
+from time import time
+import python.scores as ps
 
 class UnitHarness(Handler):
 	'''
@@ -22,5 +24,37 @@ class UnitHarness(Handler):
 			get
 			Standard get method. Renders template.
 		'''
+		self.tests=[{"name" :"Unit Test Harness", "passed" : True, "message" : "Test harness has been called correctly"}]
+		self.testScores()
 		self.render("unit.html",tests=self.tests)
+		
+
+	def addTest(self,name,passed=False,message="No Message Set"):
+		self.tests.append({"name" : name, "passed" : passed, "message" : message})
+
+	def testScores(self):
+		'''
+			testScores
+			Tests the scores.py file's Handler's methods
+		'''
+
+		test = ps.ScoresHandler()
+
+		#Retrieve the top scores
+		#use clock() on windows, time() on nix
+		sTime = time()
+		eTime = 0.0
+		try:
+			topTen,sortKey = test.getTopScores()
+			eTime = time()
+		except Exception, e:
+			self.addTest("scores.py", False, "Failed to retrieve top scores")
+		else:
+			self.addTest("scores.py", True, "Retrieved top scores in %2.5f seconds " % (eTime - sTime))
+
+		#We can't really call render methods because those will cause
+		#the gae to die.
+		
+
+
 	
