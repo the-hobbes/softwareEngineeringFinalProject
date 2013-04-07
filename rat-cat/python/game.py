be4979dbbfb6d449a7547a886305079dbd7dc01b
the-hobbes <phelan.vendeville@gmail.com>
2013-04-07 13:50:57 -0400

    removed a duplicate swap function from game.py

--- rat-cat/python/game.py
+++ rat-cat/python/game.py
@@ -657,47 +657,6 @@ class GameHandler(Handler):
 
 		return statePassedIn
 
-	'''
-		The following are utility methods, employed by the state handlers to perform various standard tasks. They are separated
-		from the handlers themselves for the sake of modularity and readability. 
-	'''
-
-	def swapCards(self, card1, card2, statePassedIn):
-		'''
-			swapCards
-			Function used to swap the image values of 2 cards. Used when a Swap power card is activated.
-			Parameters:
-				card1 and card2, the two cards taken from the playerClicks array, which must be swapped.
-				statePassedIn, the current state of the game
-			Returns:
-				statePassedIn, the gamestate modified by the swap
-		'''
-		# modify the message property of the json to explain to the player what to do.
-		statePassedIn['message']['visible'] = 1
-		statePassedIn['message']['text'] = "Choose one of your cards and one of your opponent's cards to swap."
-
-		# is card1 player or opponent? Note the format of these clicks, which are div names, for example: playerCard4 or opCard2
-		# (you can tell whats what by the first letter of the div name passed into the playerclicks array, either p or o)
-		if (card1[0] == 'p'):
-			# a player card that must be swapped out with an opponent's card
-			# get the index of this card (you can tell this by subtracting 1 from the last character of the div name passed in)
-			playerIndex = int(str(card1[-1])) - 1
-			# get the index of the other card, which must be the opponent's card
-			oppIndex = int(str(card2[-1])) - 1
-			# swap the image values of these two cards in the dictionaries, using a temp variable
-			tmpPlayerImage = statePassedIn['playCard'][playerIndex]['image']
-			statePassedIn['playCard'][playerIndex]['image'] = statePassedIn['compCard'][oppIndex]['image']
-			statePassedIn['compCard'][oppIndex]['image'] = tmpPlayerImage
-		else:
-			# a computer card that must be swapped out with a player's card. just do the reverse (change card numbers, ie card2 becomes card1)
-			playerIndex = int(str(card2[-1])) - 1
-			oppIndex = int(str(card1[-1])) - 1
-			tmpPlayerImage = statePassedIn['playCard'][playerIndex]['image']
-			statePassedIn['playCard'][playerIndex]['image'] = statePassedIn['compCard'][oppIndex]['image']
-			statePassedIn['compCard'][oppIndex]['image'] = tmpPlayerImage
-
-		return statePassedIn
-
 	def glowCards(self, drawnCard, statePassedIn):
 		'''
 			glowCards
@@ -808,7 +767,7 @@ class GameHandler(Handler):
 
 		if(knockStatus == 2):
 			statePassedIn['knockState'] = 1
-			
+
 		elif(knockStatus == 1):
 			statePassedIn['knockState'] = 0
 			statePassedIn['state'] = endGame
