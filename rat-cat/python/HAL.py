8dcfcc58f9fd34af01b37fdbbb1d5184a3af2703
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-04-11 17:28:51 -0700

    Added swap function to HAL class

--- rat-cat/python/HAL.py
+++ rat-cat/python/HAL.py
@@ -7,9 +7,13 @@
 from handler import *
 
 from random import choice
+from random import randint
+from random import random as randZ
 from google.appengine.ext import db
 import logging
 import json
+import python.gameModel as gm
+
 
 class HAL(db.Model):
 	'''
@@ -40,6 +44,7 @@ class HAL(db.Model):
 	decayMemory = db.ListProperty(float)
 	#This will be a string that builds up to tell the view what to do
 	actionsToTake = ""
+	diff =0
 
 	def __init__(self,sessionID="Debug",diff=0,aiCardArray=[],opCardArray=[],topValue=9):
 		'''
@@ -74,6 +79,134 @@ class HAL(db.Model):
 		#https://groups.google.com/forum/?fromgroups=#!topic/google-appengine/iX2fEjv5jsM ... uh
 		db.Model.__init__(self,key_name=sessionID,pkSessionID=sessionID,estAIScore=self.estAIScore,estOppScore=self.estOppScore,opCardsMem=self.opCardsMem,aiCardsMem=self.aiCardsMem,opCards=self.opCards,aiCards=self.aiCards,discardTopValue=self.discardTopValue,decayRate=self.decayRate,decayMemory=self.decayMemory)
 		self.put()
+
+	def rememberTopCard(self,topCard):
+		'''
+		rememberTopCard
+			topCard, The top card of the deck to be remembered
+		'''
+		self.discardTopValue = topCard
+
+	def doTurn(self,state):
+		'''
+		Does the HAL's turn, this function is essentially
+
+		'''
+		pass
+
+	def swapCard(self,state):
+		'''
+			Swapcard
+				This function simply switches two cards from the player and computers hands
+			Parameters:
+				state, The state of the game
+		'''
+		if(self.diff == 0):
+			#Choose a random card of the humans and ours and switch em
+			humanCard = randint(0,3)
+			compCard = randint(0,3)
+			tmp = state['playCard'][humanCard]['image']
+			state['playCard'][humanCard]['image'] = state['compCard'][compCard]['image']
+			state['compCard'][compCard]['image'] = tmp
+			#done
+		elif(self.diff == 3):
+			#Perfect Recall of all cards that it knows. This is mean
+			humanCard  = 0
+			humanValue = 15
+			i=0
+			#Look through the cards we remember and find the lowest value
+			for c in self.opCards:
+				if( int(c['image']) < humanValue):
+					humanCard = i 
+					humanValue = int(c['image'])
+				i=i+1
+			#Find out highest cards
+			compCard = 0
+			compValue = 0
+			i=0
+			for c in self.aiCards:
+				if(int(c['image']) > compValue ) :
+					compCard = i 
+					compValue = int(c['image'])
+				i=i+1
+			#Now that we know, we perform the swap
+			tmp = state['playCard'][humanCard]['image']
+			state['playCard'][humanCard]['image'] = state['compCard'][compCard]['image']
+			state['compCard'][compCard]['image'] = tmp
+			#Update our representation of the cards
+			tmp = self.opCards[humanCard]
+			self.opCards[humanCard] = self.aiCards[compCard]
+			self.aiCards[compCard] = tmp
+		else:
+			#We have a difficuly of 1 or 2, which means basic memory decay and swappage
+			#Here is our fuzzy memory scheme:
+			#The AI has some probability of having NO clue what it's and the users
+			#cards are. 
+			#So lets get the cards we think we have
+			humanCards,compCards = self.getMemory()
+			humanCard  = 0
+			humanValue = 15
+			i=0
+			#Look through the cards we remember and find the lowest value
+			for c in humanCards:
+				if( int(c['image']) < humanValue):
+					humanCard = i 
+					humanValue = int(c['image'])
+				i=i+1
+			#Find out highest cards
+			compCard = 0
+			compValue = 0
+			i=0
+			for c in compCards:
+				if(int(c['image']) > compValue ) :
+					compCard = i 
+					compValue = int(c['image'])
+				i=i+1
+			#Update our representation of it (note this propagates remembering wrong)
+			tmp = humanCards[humanCard]
+			self.opCards[humanCard] = compCard[compCard]
+			self.aiCards[compCard] = tmp
+			#Now perform the real swap
+			tmp = state['playCard'][humanCard]['image']
+			state['playCard'][humanCard]['image'] = state['compCard'][compCard]['image']
+			state['compCard'][compCard]['image'] = tmp
+
+
+
+		return state
+
+	def getMemory(self):
+		'''
+			getMemory
+				This function uses the probability of rememberance of the AI
+				to determine the representations
+		'''
+		humanRep = [0,0,0,0]
+		compRep  = [9,9,9,9]
+		for i in range(len(self.opCardsMem)):
+			if(self.opCardsMem[i] < randZ()):
+				#Remember correctly
+				humanRep[i] = self.opCards[i]
+			else:
+				#Remembered incorrectly
+				humanRep[i] = {'image' : str(randint(0,9)), 'active' : 0, 'visible' : 0}
+			if(self.aiCardsMem[i] < randz()):
+				#Remembered correctly
+				compRep[i] = self.aiCards[i]
+			else:
+				#Remembered incorrectly
+				compRep[i] = {'image' : str(randint(0,9)), 'active' : 0, 'visible' : 0}
+
+		#Return two things at once #YOLO
+		return humanRep,compRep
+
+
+
+
+
+
+
+
 
 
 
